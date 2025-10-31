6d6b91370b9c29604cd75cc20b60d7657de9edbf
PRINCE KUAMR <gmprince420@gmail.com>
2025-10-31 23:11:31 +0530

    Data fetching in React

---
+++ week-14/my-app/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="flex h-screen  items-center justify-center font-extralight tracking-widest ">
+      loading<span className="animate-pulse font-semibold">...</span>
+    </div>
+  );
+}
--- week-14/my-app/app/page.tsx
+++ week-14/my-app/app/page.tsx
@@ -1,9 +1,27 @@
-import Link from "next/link";
+interface User {
+  name: string;
+  email: string;
+}
+async function getUserData() {
+  const response = await new Promise<User>((res) => {
+   setTimeout(()=> res({
+      name: "Prince",
+      email: "gmprince420@gmail.com",
+    }), 1000)
+  });
 
-export default function Home() {
+  return response;
+}
+
+export default async function Home() {
+  const user = await getUserData();
+
   return (
-    <div>
-      Hello From <Link href={"/auth"}>Signin</Link>{" "}
+    <div className="w-full  h-screen  flex  justify-center items-center ">
+      <div className="">
+        <div className="">{user.name}</div>
+      <div className="">{user.email}</div>
+      </div>
     </div>
   );
 }
